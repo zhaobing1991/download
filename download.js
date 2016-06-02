893a3533bc8453f626dce9624301ed37867efbc1
zhaobing0908 <924540776@qq.com>
2016-06-02 18:47:53 +0800

    a

--- download.js
+++ download.js
@@ -1,5 +1,5 @@
 var main = document.querySelector("#box");
-var loc = document.querySelector("#loc");
+/*var loc = document.querySelector("#loc");*/
 var winW = window.innerWidth;
 var winH =window.innerHeight;
 
@@ -12,8 +12,8 @@ if(desW/desH<winW/winH){
     main.style.webkitTransform = 'scale('+winH/desH+')';
 }
 
-loc.addEventListener("touchstart",function(){window.location="https://itunes.apple.com/us/app/guo-shang-yi-gou/id1098039649?mt=8&uo=4";
-});
+/*loc.addEventListener("touchstart",function(){window.location="https://itunes.apple.com/us/app/guo-shang-yi-gou/id1098039649?mt=8&uo=4";
+});*/
 document.body.addEventListener('touchmove', function (event) {
     event.preventDefault();
 }, false);
