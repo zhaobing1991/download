0a72a455792649ea0d36abfee557acd273416b2d
zhaobing0908 <924540776@qq.com>
2016-06-02 18:04:53 +0800

    1

--- download.js
+++ download.js
@@ -1,6 +1,8 @@
 var main = document.querySelector("#box");
+var loc = document.querySelector("#loc");
 var winW = window.innerWidth;
 var winH =window.innerHeight;
+
 
 var desW = 640;
 var desH = 960;
@@ -9,3 +11,8 @@ if(desW/desH<winW/winH){
 }else{
     main.style.webkitTransform = 'scale('+winH/desH+')';
 }
+
+loc.addEventListener("touchstart",function(){window.location="https://itunes.apple.com/us/app/guo-shang-yi-gou/id1098039649?mt=8&uo=4";
+});
+
+
